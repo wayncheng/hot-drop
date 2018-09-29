e653402efd91a8eed9e936d5219127e8bb7004ad
wayne <wayne@che.ng>
2018-09-29 11:45:15 -0700

    added sass-loader to production build

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -168,6 +168,17 @@ module.exports = {
               compact: true,
             },
           },
+					
+					// > SCSS --------------------------------
+					{
+						test: /\.scss$/,
+						include: paths.appSrc,
+						use: [
+							require.resolve('style-loader'),
+							require.resolve('css-loader'),
+							require.resolve('sass-loader'),
+						]
+					},
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
